d85094833696fcc727c448fccd03030233395204
pedro <pedbcrespo@gmail.com>
2025-04-18 14:27:20 -0300

    adicionando config do mongo

--- configuration/config.py
+++ configuration/config.py
@@ -3,15 +3,25 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from configuration import dev_configuration as db
 from flask_cors import CORS
+from flask_pymongo import PyMongo
 import os
 
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 BASE_URL = '/city-db-api'
 
+MONGO_HOST = os.getenv("MONGO_HOST", "localhost")
+MONGO_PORT = os.getenv("MONGO_PORT", "27017")
+MONGO_DB = os.getenv("MONGO_DB", "city_database")
+
+MONGO_URI = f"mongodb://{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}"
+
 app = Flask(__name__)
 CORS(app, origins='*')
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config["MONGO_URI"] = MONGO_URI
+
 
 ormDatabase = SQLAlchemy(app)
+mongo = PyMongo(app)
 api = Api(app)
--- model/DemandLocation.py
+++ model/DemandLocation.py
@@ -1,20 +1,12 @@
 from configuration.config import ormDatabase as orm
 
-class DemandLocation(orm.Model):
-    __tablename__ = 'demand_location'
-    
-    id = orm.Column(orm.Integer, primary_key=True)
-    demand_id = orm.Column(orm.Integer, orm.ForeignKey('demand.id'))
-    district_id = orm.Column(orm.Integer, orm.ForeignKey('district.id'))
-    occurrence = orm.Column(orm.Integer)
-    
-    def __init__(self, demand_id, district_id):
+class DemandLocation:
+    def __init__(self, demand_id, street_id):
         self.demand_id = demand_id
-        self.district_id = district_id
-        self.occurrence = 0
-        
-    def __repr__(self):
-        return f"({self.id}, {self.demand_id}, {self.district_id})"
-    
+        self.street_id = street_id
+
     def json(self):
-        return {'id': self.id, 'demand_id': self.name}
+        return {
+            'demand_id': self.demand_id,
+            'street_id': self.street_id
+        }
